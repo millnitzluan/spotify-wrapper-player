376b39e1b480e8af993bbdbddbd5a13d5820e4d5
Luan Millnitz <luanmillnitz@gmail.com>
2018-01-14 23:48:03 -0200

    Add spotify-wrapper-millnitzluan module

---
+++ src/Spotify.js
@@ -0,0 +1,7 @@
+import SpotifyWrapper from 'spotify-wrapper-millnitz';
+
+const spotify = new SpotifyWrapper({
+  token: 'BQAnvsiSPkP6IEBkzCUo3ZBCZtDq7K0dA1FpCowQlql_uRII-MdcpnfseBdh34ZjjCabzI1WjJhP4weRNYQoCJ6Nt0GauOOUecE2-n90vVXneC6WHayuhrGvbV0Oab4I_agpK--MLN_IQAp7zAlg'
+});
+
+export default spotify;
--- tests/Spotify.spec.js
+++ tests/Spotify.spec.js
@@ -1,5 +1,16 @@
 import { expect } from 'chai';
+import spotify from '../src/Spotify';
 
 describe('Spotify.spec.js', () => {
+  it('should be an object', () => {
+    expect(spotify).to.been
+  });
 
+  it('should have search methods', () => {
+    expect(spotify.search).to.exist;
+  });
+
+  it('should have search album methods', () => {
+    expect(spotify.album).to.exist;
+  });
 });
