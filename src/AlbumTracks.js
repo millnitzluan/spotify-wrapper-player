865209391eea33667cca652df85ec5fd52efc380
Luan Millnitz <luanmillnitz@gmail.com>
2018-01-14 23:49:04 -0200

    Add AlbumTracks test and class

---
+++ src/AlbumTracks.js
@@ -0,0 +1,16 @@
+import converToHumanTime from './ConvertToHumanTime';
+
+function createMarkup(tracks) {
+  return tracks.map(track => `
+    <div class="music" data-track-preview="${track.preview_url}">
+      <p class="music-number">${track.track_number}</p>
+      <p class="music-title">${track.name}</p>
+      <p class="music-duration">${converToHumanTime(track.duration_ms)}</p>
+    </div>`).join('');  
+}
+
+export default function renderAlbumTracks(data, element) {
+  const markup = createMarkup(data);
+
+  element.innerHTML = markup
+}
